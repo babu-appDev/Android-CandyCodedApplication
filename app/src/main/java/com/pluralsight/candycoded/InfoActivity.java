67f9699bff21d330a238c51fb7592c6064e38931
Matt Smith <matombabs@gmail.com>
2019-10-23 18:28:07 +0300

    Update InfoActivity.java

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -40,4 +40,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Uri phony = Uri.parse("tel:0123456789");
+        Intent phoneCall = new Intent(Intent.ACTION_DIAL);
+        phoneCall.setData(phony);
+        startActivity(phoneCall);
+    }
 }
